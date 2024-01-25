c44eab8f19aadac8b8ca7d015e24df225050b248
202100351h <202100351h@utea.edu.pe>
2024-01-25 08:49:25 -0500

    caso6

--- caso/caso6.php
+++ caso/caso6.php
@@ -7,10 +7,18 @@
 </head>
 <body>
     <header>
+        <h3>Venta de productos</h3>
 
     </header>
 
     <section>
+        <form action = "caso6.php" method = "post">
+            <?php
+            error_reporting(0);
+
+            ?>
+
+        </form>
 
     </section>
     
